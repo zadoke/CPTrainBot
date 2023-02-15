b239ae461e5ecd8106c2921f953ea0e201987e88
zadoke <rafaelajs@pm.me>
2023-02-15 03:33:30 +0000

    limit autocomplete choices to 25, string splitting

--- commands/horarios.js
+++ commands/horarios.js
@@ -8,7 +8,7 @@ module.exports = {
     .setDescription('Mostra o horario dos comboios na estação dada.')
     .addStringOption((option) =>
       option.setName('nomeestacao')
-        .setDescription('O nome da estação.')
+        .setDescription('O nome da estação. É necessário escolher da lista apresentada.')
         .setRequired(true)
         .setAutocomplete(true)
     ),
@@ -17,7 +17,7 @@ module.exports = {
         // Get the user's input value
         const focusedValue = interaction.options.getFocused(true);
 
-        const response = await fetch(`https://servicos.infraestruturasdeportugal.pt/negocios-e-servicos/estacao-nome/${focusedValue.value}`, {
+        const response = await fetch(`https://servicos.infraestruturasdeportugal.pt/negocios-e-servicos/estacao-nome/${focusedValue.value.split(' ')[0]}`, {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
@@ -34,7 +34,7 @@ module.exports = {
         // Extract the station IDs from the response and format as Discord choices
         // Send the filtered choices to the user
         await interaction.respond(
-			data.response.map(station => ({
+			data.response.slice(0,25).map(station => ({
                 name: station.Nome,
                 value: station.NodeID.toString(),
                 }))
