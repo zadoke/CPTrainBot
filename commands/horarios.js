c23acd65e0202b1f652115849e65b45b89583505
zadoke <rafaelajs@pm.me>
2023-02-15 03:03:26 +0000

    added station autocomplete

--- commands/horarios.js
+++ commands/horarios.js
@@ -10,10 +10,41 @@ module.exports = {
       option.setName('nomeestacao')
         .setDescription('O nome da estação.')
         .setRequired(true)
+        .setAutocomplete(true)
     ),
+    async autocomplete(interaction){
 
-    async execute(interaction){
-        const stationName = interaction.options.getString('nomeestacao');
-        console.log(stationName);
+        // Get the user's input value
+        const focusedValue = interaction.options.getFocused(true);
+
+        const response = await fetch(`https://servicos.infraestruturasdeportugal.pt/negocios-e-servicos/estacao-nome/${focusedValue.value}`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:97.0) Gecko/20100101 Firefox/97.0'
+            }
+        });
+
+        console.log(focusedValue);
+    
+        // Call the station name endpoint with the user's input value
+        const data = await response.json();
+        
+        console.log(data);
+        // Extract the station IDs from the response and format as Discord choices
+        // Send the filtered choices to the user
+        await interaction.respond(
+			data.response.map(station => ({
+                name: station.Nome,
+                value: station.NodeID.toString(),
+                }))
+		);
+
+       
+    },
+
+    async execute(interaction) {
+        console.log(interaction.options.getString('nomeestacao'));
     }
+
 }
