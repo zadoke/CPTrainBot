d3b491c14d37de198ce1996ca142b266cee93744
zadoke <rafaelajs@pm.me>
2023-02-21 00:20:32 +0000

    add try catch, remove debug messages

--- commands/horarios.js
+++ commands/horarios.js
@@ -17,35 +17,34 @@ module.exports = {
         .setRequired(true)
 		.setDescription('O periodo de tempo que desejas consultar, em horas')
     ),
-    async autocomplete(interaction){
 
-        // Get the user's input value
-        const focusedValue = interaction.options.getFocused(true);
-
-        const response = await fetch(`https://servicos.infraestruturasdeportugal.pt/negocios-e-servicos/estacao-nome/${focusedValue.value.split(' ')[0]}`, {
+    async autocomplete(interaction) {
+        try {
+          // Get the user's input value
+          const focusedValue = interaction.options.getFocused(true);
+    
+          const response = await fetch(`https://servicos.infraestruturasdeportugal.pt/negocios-e-servicos/estacao-nome/${focusedValue.value.split(' ')[0]}`, {
             method: 'GET',
             headers: {
-                'Accept': 'application/json',
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:97.0) Gecko/20100101 Firefox/97.0'
+              'Accept': 'application/json',
+              'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:97.0) Gecko/20100101 Firefox/97.0'
             }
-        });
-
-        console.log(focusedValue);
+          });
     
-        // Call the station name endpoint with the user's input value
-        const data = await response.json();
-        
-        console.log(data);
-        // Extract the station IDs from the response and format as Discord choices
-        // Send the filtered choices to the user
-        await interaction.respond(
-			data.response.slice(0,25).map(station => ({
-                name: station.Nome,
-                value: station.NodeID.toString(),
-                }))
-		);
-
-       
+          // Call the station name endpoint with the user's input value
+          const data = await response.json();
+    
+          // Extract the station IDs from the response and format as Discord choices
+          // Send the filtered choices to the user
+          await interaction.respond(
+            data.response.slice(0, 25).map(station => ({
+              name: station.Nome,
+              value: station.NodeID.toString(),
+            }))
+          );
+        } catch (error) {
+          console.error(error);
+        }
     },
 
     async execute(interaction) {
