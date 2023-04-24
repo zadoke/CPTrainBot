f17c36d440243042775d29daab8b65e44c1c0174
zadoke <rafaelajs@pm.me>
2023-04-24 18:18:19 +0100

    feat: add interval to check train status
    - Set an interval to check the train status every 15 seconds
    - Fetch the train data from the API
    - Find the index of the user's station in the station array
    - Get the previous station
    - Check if the train has passed the previous station
    - If it has, notify the user and clear the interval

--- commands/alerta.js
+++ commands/alerta.js
@@ -1,5 +1,5 @@
 // Import necessary modules from discord.js
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const getStationNames = require('../utils/getStationNames');
 const fetchTrainDetails = require('../utils/fetchTrainDetails');
 
@@ -37,22 +37,44 @@ module.exports = {
         // If train data is invalid, reply with an error message
         return interaction.reply('O comboio não foi encontrado.');
       }
-      
-      // Find the stationId in the station array
-      const stationData = trainData.response.NodesPassagemComboio.find(station => station.NodeID === stationId);
-      
-      // See if our station is on the station array. Inform the user if it is not.
-      if (!stationData){
+
+      // Find the user's station in the stations array
+      const userStation = trainData.response.NodesPassagemComboio.find(station => station.NodeID === stationId);
+
+      // See if the user's station is on the station array. Inform the user if it is not.
+      if (!userStation){
         return interaction.reply('O comboio não passa pela a estação que introduziste. Tenta novamente!');
       }
 
-      // If ComboioPassou is true, our train already passed our station. Inform the user
-      if (stationData.ComboioPassou) {
+      // If ComboioPassou is true, our train already passed the user's station. Inform the user.
+      if (userStation.ComboioPassou) {
         return interaction.reply('O comboio já passou pela a estação!')
       }
 
+      interaction.reply(`${interaction.user.toString()}, o alerta está definido. Irei avisar-te quando o comboio ${trainNumber} chegar a ${userStation.NomeEstacao}.`);
 
-      
+      // Set an interval to check the train status every 15 seconds
+      const interval = setInterval(async () => {
+        // Fetch the train data from the API
+        const trainData = await fetchTrainDetails(trainNumber);
+
+        // Find the index of the user's station in the station array
+        const stationIndex = trainData.response.NodesPassagemComboio.findIndex(station => station.NodeID === stationId);
+
+        // Get the previous station
+        const previousStation = trainData.response.NodesPassagemComboio[stationIndex - 1];
+        
+
+        //TODO: Check if the observacoes field changes and alert the user if it does
+
+
+        // Check if the train has passed the previous station
+        if (previousStation && previousStation.ComboioPassou) {
+          // If the train has passed the previous station, notify the user and clear the interval
+          interaction.followUp(`${interaction.user.toString()}, o teu comboio está prestes a chegar.`);
+          clearInterval(interval);
+        }
+      }, 15000);
     }
 
 }
