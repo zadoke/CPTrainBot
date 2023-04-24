4b84927d5eaf6ee806f619e753fbcbb1e197f15a
zadoke <rafaelajs@pm.me>
2023-04-24 17:06:38 +0100

    feat: add train data validation and station search

--- commands/alerta.js
+++ commands/alerta.js
@@ -26,10 +26,32 @@ module.exports = {
 
     async execute(interaction){
       // Get the station name and train number from the options
-      const stationName = parseInt(interaction.options.getString('nomeestacao'));
+      const stationId = parseInt(interaction.options.getString('nomeestacao'));
       const trainNumber = interaction.options.getInteger('numerocomboio');
 
+      // Fetch the train data from the API
       const trainData = await fetchTrainDetails(trainNumber);
+
+      // Check if train data is valid
+      if (trainData.response.DataHoraDestino === null) {
+        // If train data is invalid, reply with an error message
+        return interaction.reply('O comboio não foi encontrado.');
+      }
+      
+      // Find the stationId in the station array
+      const stationData = trainData.response.NodesPassagemComboio.find(station => station.NodeID === stationId);
+      
+      // See if our station is on the station array. Inform the user if it is not.
+      if (!stationData){
+        return interaction.reply('O comboio não passa pela a estação que introduziste. Tenta novamente!');
+      }
+
+      // If ComboioPassou is true, our train already passed our station. Inform the user
+      if (stationData.ComboioPassou) {
+        return interaction.reply('O comboio já passou pela a estação!')
+      }
+
+
       
     }
 
