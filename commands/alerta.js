b2a477361a1a12780074990a4d3e07e4558775ed
zadoke <rafaelajs@pm.me>
2023-04-28 22:23:11 +0100

    feat: Add previousStatus check and send DMs for train updates
    Compare previous & current status of trains, notify users via DMs of any changes. DMs are used instead of interaction.reply due to the 15-minute token lifespan of Discord slash commands, ensuring updates are received by users.

--- commands/alerta.js
+++ commands/alerta.js
@@ -7,7 +7,7 @@ const fetchTrainDetails = require('../utils/fetchTrainDetails');
 module.exports = {
   	data: new SlashCommandBuilder()
     .setName('alerta')
-    .setDescription('Introduza o nome da estação e o número do comboio que pretende seguir.')
+    .setDescription('Introduz o nome da estação e o número do comboio que pretendes seguir.')
     .addStringOption((option) =>
       option.setName('nomeestacao')
         .setDescription('O nome da estação. É necessário escolher da lista apresentada.')
@@ -24,57 +24,61 @@ module.exports = {
       await getStationNames(interaction);
     },
 
-    async execute(interaction){
+    async execute(interaction, client){
+      // Retrieve the user ID of the user who triggered the slash command. This is required as interaction tokens for slash commands only remain valid for 15 minutes. We will use this ID to send a Direct Message to the user.
+      const userId = interaction.user.id;
+      const user = await client.users.fetch(userId);
+
       // Get the station name and train number from the options
       const stationId = parseInt(interaction.options.getString('nomeestacao'));
       const trainNumber = interaction.options.getInteger('numerocomboio');
 
-      // Fetch the train data from the API
       const trainData = await fetchTrainDetails(trainNumber);
 
       // Check if train data is valid
       if (trainData.response.DataHoraDestino === null) {
-        // If train data is invalid, reply with an error message
         return interaction.reply('O comboio não foi encontrado.');
       }
 
       // Find the user's station in the stations array
       const userStation = trainData.response.NodesPassagemComboio.find(station => station.NodeID === stationId);
 
-      // See if the user's station is on the station array. Inform the user if it is not.
+      // Check if the user's station exists in the array of train stations by evaluating the "userStation" variable. If it does not exist, inform the user that the train does not pass through the station they provided and prompt them to try again.
       if (!userStation){
         return interaction.reply('O comboio não passa pela a estação que introduziste. Tenta novamente!');
       }
 
-      // If ComboioPassou is true, our train already passed the user's station. Inform the user.
+      // Check if the train has already passed the user's station by evaluating the "ComboioPassou" property of the station data. If true, notify the user that the train has already passed their station.
       if (userStation.ComboioPassou) {
         return interaction.reply('O comboio já passou pela a estação!')
       }
 
       interaction.reply(`${interaction.user.toString()}, o alerta está definido. Irei avisar-te quando o comboio ${trainNumber} chegar a ${userStation.NomeEstacao}.`);
 
+      // Store the current train status to compare with future requests and inform the user if it changes.
+      let previousStatus = trainData.response.SituacaoComboio;
+
       // Set an interval to check the train status every 15 seconds
       const interval = setInterval(async () => {
-        // Fetch the train data from the API
         const trainData = await fetchTrainDetails(trainNumber);
 
-        // Find the index of the user's station in the station array
+        // Compare the previous status of the train with the current one. If they are different, notify the user about the update.
+        if (previousStatus && previousStatus !== trainData.response.SituacaoComboio){
+          user.send(`${interaction.user.toString()}, o estado do teu comboio mudou. Estado atual: ${trainData.response.SituacaoComboio}`); //Here we send the response outside of the interaction, for reasons that we explained above.
+        }
+
+        previousStatus = trainData.response.SituacaoComboio;
+
+        // Find the index of the user's station in the station array, this is necessary to get the previous station.
         const stationIndex = trainData.response.NodesPassagemComboio.findIndex(station => station.NodeID === stationId);
-
-        // Get the previous station
+        // Get the previous station by accessing the element in the array before the user's station.
         const previousStation = trainData.response.NodesPassagemComboio[stationIndex - 1];
-        
 
-        //TODO: Check if the observacoes field changes and alert the user if it does
-
-
-        // Check if the train has passed the previous station
+        // Check if the train has passed the previous station.
         if (previousStation && previousStation.ComboioPassou) {
-          // If the train has passed the previous station, notify the user and clear the interval
-          interaction.followUp(`${interaction.user.toString()}, o teu comboio está prestes a chegar.`);
+          user.send(`${interaction.user.toString()}, o teu comboio está prestes a chegar.`);
           clearInterval(interval);
         }
       }, 15000);
     }
-
 }
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -5,7 +5,6 @@ module.exports = {
 	async execute(interaction) {
 		if (interaction.isAutocomplete()) {
 			const command = interaction.client.commands.get(interaction.commandName);
-
 			if (!command) {
 				console.error(`No command matching ${interaction.commandName} was found.`);
 				return;
@@ -22,9 +21,8 @@ module.exports = {
 				console.error(`No command matching ${interaction.commandName} was found.`);
 				return;
 			}
-
 			try {
-				await command.execute(interaction);
+				await command.execute(interaction, interaction.client);
 			} catch (error) {
 				console.error(`Error executing ${interaction.commandName}`);
 				console.error(error);
